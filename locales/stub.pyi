0a8f7257c5720e7827cafffe7edd347ecdd3eb40
Oleg Stukalov <oleg13stukalov@gmail.com>
2025-05-08 10:51:47 +0200

    code copy from theory to all files

---
+++ locales/stub.pyi
@@ -0,0 +1,28 @@
+# from project ROOT-folder: i18n -ftl locales/en/LC_MESSAGES/txt.ftl -stub locales/stub.pyi
+from typing import Literal
+
+
+class TranslatorRunner:
+    def get(self, path: str, **kwargs) -> str: ...
+
+    hello: Hello
+    button: Button
+    no: No
+
+
+class Hello:
+    @staticmethod
+    def user(*, username) -> Literal["""Hello { $username }. Click on the button"""]: ...
+
+
+class Button:
+    @staticmethod
+    def button() -> Literal["""Button"""]: ...
+
+    @staticmethod
+    def pressed() -> Literal["""You pressed the button"""]: ...
+
+
+class No:
+    @staticmethod
+    def copy() -> Literal["""This type of update is not supported by the send_copy method"""]: ...
